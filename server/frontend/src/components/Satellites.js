492a3734e5f8747988fbaabc38f0bf2f84ed866a
Martin Krauskopf <martin.krauskopf@gmail.com>
2018-12-02 10:46:24 +0100

    Switch satellites to the react-virtualized table.

--- server/frontend/src/components/Satellites.js
+++ server/frontend/src/components/Satellites.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import './responsive.css';
+import { Column, Table } from 'react-virtualized';
 
 const Satellites = (props) => {
+
+  const _rowClassName = ({index}) => {
+    if (index % 2 === 0) {
+      return "oddRow";
+    }
+  }
+
   if (props.satellites.length === 0) {
     return <h2>Planet <i>{props.planetName}</i> does not have any satellites.</h2>
   } else { // render table with satellites
@@ -9,32 +16,21 @@ const Satellites = (props) => {
       <div>
         <h2>Satellites of <i>{props.planetName}</i></h2>
 
-        <table className="rwd-table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>GM</th>
-              <th>Radius</th>
-              <th>Density</th>
-              <th>Magnitude</th>
-              <th>Albedo</th>
-            </tr>
-          </thead>
-          <tbody>
-          {
-            props.satellites.map(satellite => {
-              return <tr key={satellite.id}>
-                       <td>{satellite.name}</td>
-                       <td>{satellite.gm}</td>
-                       <td>{satellite.radius}</td>
-                       <td>{satellite.density}</td>
-                       <td>{satellite.magnitude}</td>
-                       <td>{satellite.albedo}</td>
-                     </tr>
-            })
-          }
-          </tbody>
-        </table>
+        <Table width={515}
+               height={514}
+               headerHeight={90}
+               rowHeight={40}
+               rowCount={props.satellites.length}
+               rowGetter={({ index }) => props.satellites[index]}
+               rowClassName={_rowClassName}
+               >
+          <Column label='Name' dataKey='name' width={105} />
+          <Column label='GM' dataKey='gm' width={85} />
+          <Column label='Radius' dataKey='radius' width={75} />
+          <Column label='Density' dataKey='density' width={75} />
+          <Column label='Magnitude' dataKey='magnitude' width={105} />
+          <Column label='Albedo' dataKey='albedo' width={70} />
+        </Table>
 
       </div>
     )
