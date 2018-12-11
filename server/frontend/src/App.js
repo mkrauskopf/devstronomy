71604b277528f7d3139e831622540cc22e2a420d
Martin Krauskopf <martin.krauskopf@gmail.com>
2018-12-11 15:03:51 +0100

    Initial works on the switch to Material UI library.

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,15 +1,91 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Planets from './components/Planets';
-import './App.css';
+import PropTypes from 'prop-types';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Planets/>
-      </div>
-    );
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+import amber from '@material-ui/core/colors/amber';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import LaunchIcon from '@material-ui/icons/Launch';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: { main: grey[900] },
+    secondary: { main: amber[500] },
   }
+});
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  launchIcon: {
+    width: 13,
+    height: 13,
+    color: '#66a',
+  },
+});
+
+function App(props) {
+  const { classes } = props;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div>
+
+        <div className={classes.root}>
+          <AppBar position='static'>
+            <Toolbar variant='dense'>
+
+              <Button variant='text' className={classes.button}>
+                Interactive Demo
+              </Button>
+              <Button variant='text' className={classes.button}>
+                Datasets
+              </Button>
+
+              <div className={classes.grow} />
+
+              <Button variant='text'
+                      className={classes.button}
+                      target='_blank'
+                      href='https://github.com/mkrauskopf/devstronomy/'>
+                GitHub&nbsp;<LaunchIcon className={classes.launchIcon} />
+              </Button>
+
+            </Toolbar>
+          </AppBar>
+        </div>
+
+        <div>
+          <div className='content'>
+            This is an interactive demo of Devstronomy datasets. See <a href='/#datasets'>datasets</a> section for more
+            details.
+            <p>
+              Click the planet to see only its satellites.
+            </p>
+          </div>
+
+          <hr/>
+
+          <div className='planets'>
+            <Planets/>
+          </div>
+        </div>
+
+      </div>
+    </MuiThemeProvider>
+  );
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
+
