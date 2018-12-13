ade307dd8c9afe16c210c61ced900e5d1689250f
Martin Krauskopf <martin.krauskopf@gmail.com>
2018-12-13 20:47:34 +0100

    Updates

---
+++ precache-manifest.47ddcad7b7925d50b99f171720bf38c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a763091a73c5c2abd58b",
+    "url": "/devstronomy/static/css/main.e967d0ee.chunk.css"
+  },
+  {
+    "revision": "a763091a73c5c2abd58b",
+    "url": "/devstronomy/static/js/main.a763091a.chunk.js"
+  },
+  {
+    "revision": "f10494aad65049a83d60",
+    "url": "/devstronomy/static/js/1.f10494aa.chunk.js"
+  },
+  {
+    "revision": "2b48f42124e5de9d9800",
+    "url": "/devstronomy/static/js/runtime~main.2b48f421.js"
+  },
+  {
+    "revision": "9fc619add0b0633682d4d99c6af4b60b",
+    "url": "/devstronomy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/devstronomy/precache-manifest.8c27ecfcddeaf80261bd8959d78c9be0.js"
+  "/devstronomy/precache-manifest.47ddcad7b7925d50b99f171720bf38c0.js"
 );
 
 workbox.clientsClaim();
