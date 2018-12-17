9bea47cef713fc1d6052fafa467a514805e37471
Martin Krauskopf <martin.krauskopf@gmail.com>
2018-12-17 20:43:37 +0100

    Switch to React Router.

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 import Planets from './components/Planets';
 import Datasets from './components/Datasets';
@@ -35,61 +37,51 @@ const styles = theme => ({
   },
 });
 
-const Content = (prop) => {
-  if (prop.contentKey === 'planets') {
-    return (
-      <div>
-        <div className='contentCenter'>
-          This is an interactive version of <a href='https://nssdc.gsfc.nasa.gov/planetary/factsheet/'>Planetary Fact
-          Sheet</a> and <a href='https://ssd.jpl.nasa.gov/?sat_phys_par'>Planetary Satellite Physical Parameters</a>
-          &nbsp;datasets from {Links.jpl}. See datasets section for more details.
-          <p>
-            <span className='highlight'>Select the planet below</span> to see only its satellites.
-          </p>
-        </div>
-
-        <hr/>
-        <div className='content'>
-          <span className='warning'>Note:</span> the following is not migrated from original satellite dataset yet:
-          <ul>
-            <li>For <em>Magnitude</em> column in satellites dataset V<sub>0</sub> or R are not distinguished.</li>
-            <li>
-              <em>Uncertainties</em> for some values. For example, for original value <code>13.70±0.04</code> in JPL
-              dataset we have just <code>13.70</code> without <code>0.04</code> uncertainty.
-            </li>
-          </ul>
-        </div>
-
-        <hr/>
-        <div className='planets'>
-          <Planets/>
-        </div>
+const PlanetsContent = () => {
+  return (
+    <div>
+      <div className='contentCenter'>
+        This is an interactive version of <a href='https://nssdc.gsfc.nasa.gov/planetary/factsheet/'>Planetary Fact
+        Sheet</a> and <a href='https://ssd.jpl.nasa.gov/?sat_phys_par'>Planetary Satellite Physical Parameters</a>
+        &nbsp;datasets from {Links.jpl}. See datasets section for more details.
+        <p>
+          <span className='highlight'>Select the planet below</span> to see only its satellites.
+        </p>
       </div>
-    );
-  } else if (prop.contentKey === 'datasets') {
-    return (
-      <div className='datasets'>
-        <Datasets/>
+
+      <hr/>
+      <div className='content'>
+        <span className='warning'>Note:</span> the following is not migrated from original satellite dataset yet:
+        <ul>
+          <li>For <em>Magnitude</em> column in satellites dataset V<sub>0</sub> or R are not distinguished.</li>
+          <li>
+            <em>Uncertainties</em> for some values. For example, for original value <code>13.70±0.04</code> in JPL
+            dataset we have just <code>13.70</code> without <code>0.04</code> uncertainty.
+          </li>
+        </ul>
       </div>
-    );
-  }
-}
 
-class App extends React.Component {
+      <hr/>
+      <div className='planets'>
+        <Planets/>
+      </div>
+    </div>
+  );
+};
 
-  state = {
-    contentKey: 'planets'
-  }
+const DatasetsContent = () => {
+  return (
+    <div className='datasets'>
+      <Datasets/>
+    </div>
+  );
+};
 
-  switchContent(contentKey) {
-    this.setState({
-      contentKey: contentKey
-    })
-  }
+const App = (props) => {
 
-  render() {
-    const classes = this.props.classes;
-    return (
+  const { classes } = props;
+  return (
+    <Router>
       <MuiThemeProvider theme={theme}>
         <div>
 
@@ -97,10 +89,10 @@ class App extends React.Component {
             <AppBar position='static'>
               <Toolbar variant='dense'>
 
-                <Button variant='text' className={classes.button} onClick={() => this.switchContent('planets')} >
-                  Interactive Demo
+                <Button variant='text' className={classes.button} component={Link} to='/planets'>
+                  Planets & Satellites
                 </Button>
-                <Button variant='text' className={classes.button} onClick={() => this.switchContent('datasets')} >
+                <Button variant='text' className={classes.button} component={Link} to='/datasets'>
                   Datasets
                 </Button>
 
@@ -117,15 +109,16 @@ class App extends React.Component {
             </AppBar>
           </div>
 
-          <div>
-            <Content contentKey={this.state.contentKey} />
-          </div>
+          <Route path='/' exact component={PlanetsContent} />
+          <Route path='/planets/' component={PlanetsContent} />
+          <Route path='/datasets/' component={DatasetsContent} />
 
         </div>
-      </MuiThemeProvider>);
-  }
+      </MuiThemeProvider>
+    </Router>
+  );
 
-}
+};
 
 App.propTypes = {
   classes: PropTypes.object.isRequired,
